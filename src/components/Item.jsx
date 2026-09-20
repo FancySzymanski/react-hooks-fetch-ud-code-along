04567de481f8580d24503f510c68920fe286873f
Alex Szymanski <alex.szy.2@gmail.com>
2026-09-19 21:42:43 -0400

    Update and Delete functionality added

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,49 @@
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+  function handleDeleteClick() {
+     fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then(r => {
+      if (r.ok) {
+        return r.json()
+      } else {
+        console.log("failed to delete item")
+      }
+    })
+    .then(() => onDeleteItem(item))
+    .catch(error => console.log(error)) 
+  }
+  function handleAddToCartClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      isInCart: !item.isInCart,
+    }),
+  })
+    .then(r => {
+      if (r.ok) {
+        return r.json()
+      } else {
+        console.log("failed to update item")
+      }
+    })
+    .then(updatedItem => onUpdateItem(updatedItem))
+    .catch(error => console.log(error))
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button className={item.isInCart ? "remove" : "add"} onClick={handleAddToCartClick}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick}>
+        Delete
+        </button>
     </li>
   );
 }
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -20,6 +20,22 @@ function ShoppingList() {
       .catch(error => console.log(error))
   }, []);
 
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter(item => item.id !== deletedItem.id);
+  setItems(updatedItems);
+  }
+
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map(item => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems)
+  }
+
   function handleAddItem(newItem) {
     setItems([...items, newItem]);
   }
@@ -43,7 +59,11 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item 
+          key={item.id} 
+          item={item} 
+          onUpdateItem={handleUpdateItem}
+          onDeleteItem={handleDeleteItem} />
         ))}
       </ul>
     </div>
